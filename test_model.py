486ed813f1da9738b596ffbf345dab926ef25f2a
Tal Shiran <talshiran0000@gmail.com>
2017-06-12 20:33:42 +0300

    add test model script

--- test_model.py
+++ test_model.py
@@ -13,7 +13,7 @@ user = 'tal'
 
 klr = 3  # in percentage
 seg_ratio = 0.75
-batch_size = 1000
+batch_size = 1338
 display_step = 10
 min_epochs = 1000
 n_classes = 3
@@ -75,6 +75,8 @@ with tf.Session() as sess:
         img_dims = tmp_img.GetSize()
         img_size = img_dims[0] * img_dims[1] * img_dims[2]
 
+        seg_tmp = stk.ReadImage('C:\\CT\\Test\\Segmentations\\10000100_1_CTce_ThAb_58_6.nii')
+        print("set_tmp - 58_6.nii")
 
         batch_x, batch_y = tf.train.batch([x_data, y_data],
                                           batch_size=[batch_size],
@@ -86,14 +88,36 @@ with tf.Session() as sess:
         threads = tf.train.start_queue_runners(sess=sess, coord=coord)
 
         epochs = 1
+        z_index = 0
+        count_iters = 0
+        x_index, y_index = 0, 0
         while not coord.should_stop():
             try:
                 batch_x_eval, batch_y_eval = sess.run([batch_x, batch_y])
 
                 # predicted_seg = np.zeros(x_data.size, dtype=np.uint8)
-                predicted_seg = np.zeros(img_size, dtype=np.uint8)
+                # predicted_seg = np.zeros(img_size, dtype=np.uint8)
                 acctual_prediction = tf.argmax(pred, axis=1)
                 ap_eval = sess.run([acctual_prediction], feed_dict={x: batch_x_eval, y: batch_y_eval})
+
+                for val in ap_eval[0]:
+                    count_iters += 1
+                    print(count_iters)
+                    for p_height in range(14):
+                        for p_width in range(14):
+                            value = 1 if val >= 1 else 0
+                            #print('x: ' + str(i), ' y: ', str(j), ' z: ', str(z_index))
+                            #print('patch height: ', str(p_height), ' patch_width: ', str(p_width))
+                            #print('value: ', str(value))
+                            seg_tmp.SetPixel((x_index*14)+p_width, (y_index*14)+p_height, z_index, value)
+                            x_index += 1
+                        x_index = 0
+                        y_index += 1
+                    x_index = 0
+                    y_index = 0
+                stk.WriteImage(seg_tmp, 'Predicted_Segmentations\\' + class_f + '_pseg.nii.gz')
+                print('Predicted segmentation has saved.')
+                z_index += 1
 
                 if epochs % 10 == 0:
                     loss, acc = sess.run([cost, accuracy], feed_dict={x: batch_x_eval, y: batch_y_eval})
@@ -101,20 +125,37 @@ with tf.Session() as sess:
                           "{:.6f}".format(loss / batch_size) + ", Test Accuracy = " + \
                           "{:.5f}".format(acc))
 
-                if epochs % 50 == 0:
                     # create predicted segmentation
-                    for k, v in enumerate(ap_eval):
-                        for j in range(k * 196, (k + 1) * 196):
-                            predicted_seg[j] = 1 if ap_eval[0][v][j] >= 1 else 0
-                            print(predicted_seg[j])
+                    # z_index, i, j = 0, 0, 0
+                    # i_patch, j_patch = 0, 0
+                    # for k, v in enumerate(ap_eval[0]):
+                    #         if i == 13:
+                    #             i = 0
+                    #             j += 1
+                    #         # if v == 0:
+                    #         #     continue
+                    #         # predicted_seg[z_index, (i+k)+(i_patch*14):((i+k)+(i_patch*14))+14, (j*14)+(j_patch*14):(j*14)+(j_patch*14)+14] = 1 if v >= 1 else 0
+                    #         #seg_tmp.SetPixel(z_index, (i+k)+(i_patch*14):((i+k)+(i_patch*14))+14, (j*14)+(j_patch*14):(j*14)+(j_patch*14)+14, 1 if v >= 1 else 0)
+                    #         #print('z: ', z_index)
+                    #         #print('x: ', (i+k)+(i_patch*14), ' ', ((i+k)+(i_patch*14))+14)
+                    #         #print('y: ', (j*14)+(j_patch*14), ' ', (j*14)+(j_patch*14)+14)
+                    #         if k == img_dims[0]-1:
+                    #             j_patch += 1
+                    #         if i_patch == 37:
+                    #             i_patch = 0
+                    #             j_patch += 1
+                    #         if i_patch == img_dims[0]-1 and j_patch == img_dims[1]-1:
+                    #             i_patch, j_patch = 0
+                    #             z_index += 1
+                    #         i_patch += 1
 
                     # predicted_seg = predicted_seg.reshape(predicted_seg.shape[0], 1)
-                    predicted_seg = predicted_seg.reshape(img_dims[2],img_dims[0],img_dims[1])
-                    stk_img = stk.GetImageFromArray(predicted_seg, isVector=False)
+                    # predicted_seg = predicted_seg.reshape(img_dims[2],img_dims[0],img_dims[1])
+                    # stk_img = stk.GetImageFromArray(predicted_seg, isVector=False)
                     # stk_img = stk.Cast(stk_img, stk.sitkUInt8)
                     # stk_img = stk.Shrink(stk_img, [1, 2, 1])
-                    stk.WriteImage(stk_img, 'Predicted_Segmentations\\' + class_f + '_pseg.nii.gz')
-                    print('Predicted segmentation has saved.')
+                    # stk.WriteImage(seg_tmp, 'Predicted_Segmentations\\' + class_f + '_pseg.nii.gz')
+                    # print('Predicted segmentation has saved.')
 
                 epochs += 1
 
